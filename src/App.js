b74403fef65c28463282e141a0c8e518bfc6c708
0xNunana <kudayapaul@gmail.com>
2023-02-17 16:38:01 +0000

    mobile view image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
-
+import {AiOutlineShoppingCart} from "react-icons/ai"
 
 function App() {
   return (
-    <div >
-      Innitial
+    <div className="flex justify-center bg-rose-50">
+      <div className="w-100">
+        <div className="p-10">
+          <img className="rounded-t-lg" src="/image-product-mobile.jpg" width={350}/>
+        
+        
+        </div>
+        
+          
+       
+        
+      </div>
     </div>
   );
 }
