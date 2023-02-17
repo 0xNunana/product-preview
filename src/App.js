6cf3b0df43410b1a104c2be23679083020bf1d49
0xNunana <kudayapaul@gmail.com>
2023-02-17 18:34:10 +0000

    Final styling

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import {AiOutlineShoppingCart} from "react-icons/ai"
 function App() {
   return (
     <div className="flex justify-center bg-rose-50">
-<div className="grid md:grid-cols-2 sm:grid-cols-1 py-10">
-  <div className="hidden md:inline-block" >
+<div className="grid md:grid-cols-2 sm:grid-cols-1 py-10 ">
+  <div className="hidden md:inline-block " >
     <img className="rounded-l-lg" src="/image-product-desktop.jpg" width={350} />
     
   </div>
-  <div className="md:hidden">
+  <div className="md:hidden ">
     <img className="rounded-t-lg" src="/image-product-mobile.jpg" width={350} />
     
   </div>
   <div>
-    <div className="  p-5 border bg-white md:hidden">
+    <div className="  p-5 border rounded-b-lg bg-white md:hidden">
           <p className="text-sm text-gray-500 pb-1">PERFUME</p>
           <p className="text-4xl pb-3 font-New">Gabrielle Essence<br/> Eau De Parfum</p>
           <p className="text-gray-500 ">A floral, solar and voluptuous <br/>interpretation composed by Olivier <br/>Polge, Perfumer-Creator for the House of <br/>CHANEL.</p>
