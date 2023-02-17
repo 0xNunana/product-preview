e7a20d395e3de186c257f00653a740ca35ca0df8
0xNunana <kudayapaul@gmail.com>
2023-02-17 17:12:20 +0000

    style change

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
         <div className="p-10">
           <img className="rounded-t-lg" src="/image-product-mobile.jpg" width={350}/>
         <div className="p-5 border bg-white">
-          <p>PERFUME</p>
-          <p className="text-4xl pb-3 font-semibold">Gabrielle Essence<br/> Eau De Parfum</p>
-          <p className="text-gray-400 ">A floral, solar and voluptuous <br/>interpretation composed by Olivier <br/>Polge, Perfumer-Creator for the House of <br/>CHANEL.</p>
+          <p className="text-sm text-gray-500 pb-1">PERFUME</p>
+          <p className="text-4xl pb-3 font-New">Gabrielle Essence<br/> Eau De Parfum</p>
+          <p className="text-gray-500 ">A floral, solar and voluptuous <br/>interpretation composed by Olivier <br/>Polge, Perfumer-Creator for the House of <br/>CHANEL.</p>
         <div className="flex pt-5 space-x-5 pb-2">
-          <h1 className="text-3xl">$149.99</h1>
-          <p className="text-sm pt-2 line-through">$169.99</p>
+          <h1 className="text-3xl text-emerald-700/90 font-New">$149.99</h1>
+          <p className="text-sm  text-gray-500 pt-2 line-through">$169.99</p>
         </div>
-        <div className="bg-green-300 flex justify-center p-2 rounded-lg my-2">
-          <button className="flex p-2">
+        <div className="bg-emerald-700/90 flex justify-center p-2 rounded-lg my-2">
+          <button className="flex  text-white p-1">
             <p className="pt-1 mr-2"><AiOutlineShoppingCart/></p>
             <p>Add to Cart</p> 
             </button>
